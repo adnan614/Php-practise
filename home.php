52c3a736ebc4eb20b927d68bd5ba73fb58eac1b2
Adnan faruque Alvi <17103169@iubat.edu>
2020-09-10 17:41:24 +0600

    third commit

--- home.php
+++ home.php
@@ -1,25 +1,25 @@
-<pre>
+
 
 <?php
 
-$pro = $_FILES['profile'];
+// $pro = $_FILES['profile'];
 
- $name = $pro['name'];
- $type = $pro['type'];
- $tmp_name = $pro['tmp_name'];
+//  $name = $pro['name'];
+//  $type = $pro['type'];
+//  $tmp_name = $pro['tmp_name'];
 
-if(!empty($name)){
-    $location = "upload/";
-    if(move_uploaded_file($tmp_name,$location. $name)){
-        echo "File upload Successfully.<br>";
-        $path=$location. $name;
-        echo "<img src='$path' width='200' height='300'>"; 
-    }else{
-        echo "Failed";
-    }
-}else{
-    echo"file not found";
-}
+// if(!empty($name)){
+//     $location = "upload/";
+//     if(move_uploaded_file($tmp_name,$location. $name)){
+//         echo "File upload Successfully.<br>";
+//         $path=$location. $name;
+//         echo "<img src='$path' width='200' height='300'>"; 
+//     }else{
+//         echo "Failed";
+//     }
+// }else{
+//     echo"file not found";
+// }
 
 
 // if($usr=="google"){
@@ -30,5 +30,27 @@ if(!empty($name)){
 // }else{
 //     echo "you didn't redirect";
 // }
+
+// $usrname = $_REQUEST['username'];
+// $usremail = $_REQUEST['email'];
+// $usrpassword = $_REQUEST['password'];
+
+// // $countpass = strlen($usrpassword);
+
+// //    if(!($countpass >=5 && $countpass <=10 )){
+// //          header("location: test.php?wrongpass= You entered this password = $usrpassword.");
+// //    }else{
+// //      echo "perfect";
+// //    }
+
+// $hash_format = "$2y$07$";
+// $salt = "sdfgjghdfhjgdfh22dgshfhjdfhjgh";
+
+// $conC = $hash_format . $salt;
+
+// echo  $usrpassword;
+// echo "<br>";
+// echo crypt($usrpassword ,$conC);
+
 ?>
 
--- test.php
+++ test.php
@@ -1,7 +1,28 @@
-<form action="home.php"  method="post" enctype="multipart/form-data">
+<!-- <form action="home.php"  method="post" enctype="multipart/form-data">
 
       <input type="file" name="profile"><br>
       
       <input type="submit" name="submit" value="upload">
 
-</form>
+ </form>  -->
+
+ <!-- <form action="home.php" method="get"> 
+ 
+       <input type="text" name="username" placeholder="username"><br>
+      <input type="email" name="email" placeholder="email"><br>
+      <input type="password" name="password" placeholder="password"><br>   
+      <input type="submit" name="submit" value="submit">
+ 
+ 
+ </form> -->
+
+
+  <?php
+
+// //   if(isset($_REQUEST['wrongpass'])){
+// //         echo $_REQUEST['wrongpass'];
+//   }
+ 
+
+
+?> 
